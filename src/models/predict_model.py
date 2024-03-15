9572207ff78c3aba502ceae3f9a4fbc970de660b
Tenzin Kalden <albertkalden@gmail.com.com>
2024-03-14 20:32:10 -0400

    show source link in the answer text

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,12 +1,7 @@
-from langchain.vectorstores import Pinecone
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.chat_models import ChatOpenAI
 from openai import OpenAI
 from sentence_transformers import SentenceTransformer
 import torch
 from src.util.DLAIUtils import Utils
-import time
 import logging
 
 
@@ -22,41 +17,20 @@ class PredictionModel:
         self.context_limt=10000
 
 
-    def qa_with_sources(self, query: str):
-        llm = ChatOpenAI(
-            openai_api_key=self.utils.get_openai_api_key(),
-            model_name='gpt-3.5-turbo',
-            temperature=0.0
-        )
-        embed = OpenAIEmbeddings(  model='text-embedding-ada-002', openai_api_key=self.utils.get_openai_api_key())
-        text_field = "text"  # the metadata field that contains our text
-        # initialize the vector store object
-        vector_store =  Pinecone(
-            self.index, embed.embed_query, text_field
-        )
-        qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
-            llm=llm,
-            chain_type="stuff",
-            retriever=vector_store.as_retriever()
-        )
-        return qa_with_sources(query)
-
     def search_relevant_text(self, query: str):
         logging.info('Searching for relevant text for: %s', query)     
         results = self.index.query(top_k=10, vector=self.model.encode(query).tolist(), include_metadata=True, include_values=False)
         top_results = []
         for result in results['matches']:
-            while len(top_results) < 3: #returning top 3 relevant text
-             if(result['score'] > 0.5):
+             if(result['score'] > 0.5 and len(top_results) < 3): #top 3 results
                 top_results.append(result['metadata'])
                 logging.info('Score: %s | Result: %s', result['score'], result['metadata'])
                 logging.info('='*100)
-            break
         return top_results
    
     def generate_prompt_openai(self, query: str, relevant_text: list):
          # get relevant contexts
-        contexts = [item['text'] for item in relevant_text]    
+        contexts = [item['text'] for item in relevant_text]   
         if contexts == []:
             return {'answer':"No Relevant Information Found. Please upload some more information to know the answer to your question."}  
         # build our prompt with the retrieved contexts included
